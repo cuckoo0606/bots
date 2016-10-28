98cb2abe391ca332399c291181c73ac44424a479
zhouguang <zhouguang@sofyun.com>
2016-10-28 15:29:02 +0800

    个人中心页面改版

--- www/js/controller/profile.js
+++ www/js/controller/profile.js
@@ -1,10 +1,11 @@
 angular.module('starter.controllers')
 
 .controller('ProfileCtrl', function($scope, $rootScope, $ionicModal, $ionicLoading, $timeout, $sce,
-            UserService, OrderService, CloseOrderService, AppConfigService, CapitalService,AccountService) {
+            UserService, OrderService, CloseOrderService, AppConfigService, CapitalService) {
     $scope.order_list = OrderService.order_list;
     $scope.close_order_list = CloseOrderService.order_list;
-	$scope.account = AccountService.account;
+    console.log($rootScope.user);
+	$scope.account = $rootScope.user;
     $scope.pay_modal_url = "";
     $scope.deposit_bank_list = [];
     $scope.bank_list = AppConfigService.bank_list;
