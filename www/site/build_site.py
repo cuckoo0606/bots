1a75bce33ecf766c65fc2b991f6b8dfe87a7a16d
lixingtie <self@lixingtie.me>
2016-11-22 18:19:47 +0800

    修改打包APP配置

--- www/site/build_site.py
+++ www/site/build_site.py
@@ -19,7 +19,6 @@ os.system("echo '    Application.start(port=8085)' >> app.py")
 # 生成静态目录
 os.system("rm -rf static")
 os.system("mkdir static")
-os.system("cp ../favicon.ico static/")
 os.system("cp -r ../img/ static/img/")
 os.system("cp -r ../css/ static/css/")
 os.system("cp -r ../js/ static/js/")
--- www/site/settings.py
+++ www/site/settings.py
@@ -1,11 +1,14 @@
 #!/usr/lib/env python
 # -*- encoding:utf-8 -*-
 
+#系统端口
+TORNADO_PORT=8080
+
 # 系统域名
-SERVER_DOMAIN = ""
+SERVER_DOMAIN = "http://wp.sofyun.com"
 
 # 交易接口地址
-TRADE_API_URL = ""
+TRADE_API_URL = "http://120.24.44.211:4002/v1/"
 
 # MONGODB链接地址
 MONGODB_HOST = "localhost"
@@ -15,12 +18,12 @@ MONGODB_PORT = 27017
 MONGODB_DB = "bots"    
 
 # 微信支付应用密钥
-WECHAT_APP_SECRET = ""
+WECHAT_APP_SECRET = "ef01c05bafa4c8679cb61c3e7b986f4e"
 # 微信支付加密秘钥
-WECHAT_SINGN_KEY = ""
+WECHAT_SINGN_KEY = "da92df82ad7211e69e8ea8667f14e6b5"
 # 微信分配的公众账号ID
-WECHAT_PAY_APPID = ""
+WECHAT_PAY_APPID = "wx7ab74ddae5651fc8"
 # 微信支付分配的商户号
-WECHAT_PAY_MCHID = ""
+WECHAT_PAY_MCHID = "1307323601"
 # 调用微信支付服务器的IP地址
-WECHAT_PAY_CREATE_IP = ""
+WECHAT_PAY_CREATE_IP = "120.24.44.211"
